1c7c030df43bc2df413c9bca7abf9dda33e5e38e
DS <dsavitskiy@tile-devel1.rian.off>
2026-02-19 17:29:52 +0300

    fix: treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,26 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  opts_extend = { "ensure_installed" },
+  opts = {
+    indent = { enable = true },
+    highlight = { enable = true },
+    matchup = { enable = true },
+    folds = { enable = true },
+    ensure_installed = {
+     "css",
+     "html",
+     "python",
+     "lua",
+     "json",
+     "jsonc",
+     "markdown",
+     "markdown_inline",
+     "regex",
+     "vim",
+     "xml"
+   }
+  }
 }
 
