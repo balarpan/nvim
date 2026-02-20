25590cbcbfa5180a154df53b0d4599011f22f4e9
DS <dsavitskiy@tile-devel1.rian.off>
2026-02-20 11:24:46 +0300

    fix: ftplugin folder

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,3 +1,6 @@
+setlocal softtabstop=4
+let b:undo_ftplugin = "setl softtabstop<"
+
 vim.treesitter.start()
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo[0][0].foldmethod = 'expr'
