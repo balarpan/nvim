525e78a44b769b0b627f991fcb92c327db8489bd
DS <dsavitskiy@tile-devel1.rian.off>
2026-02-24 15:11:55 +0300

    fix: ftplugin/python.lua

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,5 +1,9 @@
-setlocal softtabstop=4
-let b:undo_ftplugin = "setl softtabstop<"
+vim.opt_local.autoindent = true
+vim.opt_local.tabstop = 4       -- 1 tab is 4 spaces
+vim.opt_local.expandtab = true  -- Use spaces for tabs
+vim.opt_local.shiftwidth = 4    -- Indent/outdent by 4 spaces
+vim.opt_local.softtabstop = 4   -- Backspace over indentation as 4 spaces
+-- vim.opt_local.fileformat = 'unix'
 
 vim.treesitter.start()
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
