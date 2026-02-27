1a7334d35ecfcc8d045e856735a4562eac3398b9
DS <balarpan@gmail.com>
2026-02-28 02:26:24 +0300

    fix: color-theme

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -6,6 +6,6 @@ vim.opt_local.softtabstop = 4   -- Backspace over indentation as 4 spaces
 -- vim.opt_local.fileformat = 'unix'
 
 vim.treesitter.start()
-vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo[0][0].foldmethod = 'expr'
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = "main", -- Required for 0.11
   lazy = false,
   build = ':TSUpdate',
 
@@ -14,9 +15,28 @@ return {
   },
   config = function(_, opts)
     require("nvim-treesitter").setup(opts)
+    local languages = opts.ensure_installed
     -- you still can launch installation manually
     -- Example: :TSInstall html css printf comment
-    require('nvim-treesitter').install(opts.ensure_installed)
+    require('nvim-treesitter').install(languages)
+
+    -- Treesitter features for installed languages must be enabled manually
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = languages,
+      callback = function()
+        -- Enable native Neovim treesitter highlighting
+        vim.treesitter.start()
+
+        -- Configure code folding
+        -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        -- vim.wo.foldlevel = 99
+
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
   end,
 }
 
