f7511324916f754ffd55bc903eedea44da17033b
DS <balarpan@gmail.com>
2026-03-25 00:28:38 +0300

    fix: colorscheme nightfox

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,10 +29,14 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "plugins.colorscheme" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "nightfox" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+-- vim.cmd [[colorscheme kanagawa]]
+vim.cmd [[colorscheme nightfox]]
+
