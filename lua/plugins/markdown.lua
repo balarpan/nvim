65e15686894c8544aa00d14b1dafe7aa6a0487e8
DS <dsavitskiy@tile-devel1.rian.off>
2026-03-30 11:34:04 +0300

    fix: markdown plugin typo

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,17 @@
-return { "timantipov/md-table-tidy.nvim",
+return {
+  {
+    "timantipov/md-table-tidy.nvim",
     -- default config
     opts = {
       padding = 1,        -- number of spaces for cell padding
       key = "<leader>tt", -- key for command :TableTidy<CR>
-    },
-    {
-      'Kicamon/markdown-table-mode.nvim',
-      config = function()
-        require('markdown-table-mode').setup()
-      end
     }
+  },
+  {
+    'Kicamon/markdown-table-mode.nvim',
+    config = function()
+      require('markdown-table-mode').setup()
+    end
+  }
 }
 
