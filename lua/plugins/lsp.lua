3300cac6f6f9d5027732c96aa68e460b421c07c6
DS <balarpan@gmail.com>
2026-04-15 01:25:52 +0300

    feat(lsp): lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,6 +37,18 @@ return {
         capabilities = capabilities,
       })
     end,
-  }
+  },
+  {
+    -- For properly handle vim.bla-bla-bla
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 }
 
